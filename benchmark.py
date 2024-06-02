b7996f3dd1767b79c27806647c64c544741050d4
ec891900 <agbeto.kossi_wilfried@courrier.uqam.ca>
2024-06-02 18:30:12 -0400

    fix benchmark

--- benchmark.py
+++ benchmark.py
@@ -8,13 +8,18 @@ from memory_profiler import profile
 from memory_profiler import memory_usage
 import threading
 import multiprocessing
-import csv    
+import csv
+import random
+    
 #
 def generate_erdos_renyi_graph(n, p=0.1, directed=False):
-    """_summary_
-    n : Le nombre de nœuds dans le graphe.
-    p : La probabilité pour chaque paire de nœuds d'être connectée par une arête.
-    directed (optionnel) : Si True, crée un graphe dirigé
+    """
+        Generates an Erdős-Rényi graph.
+
+        Parameters:
+        n (int): The number of nodes in the graph.
+        p (float): The probability for each pair of nodes to be connected by an edge.
+        directed (bool, optional): If True, creates a directed graph. Defaults to False.
     """
     if directed:
         return nx.gnp_random_graph(n, p, directed=True)
@@ -22,41 +27,49 @@ def generate_erdos_renyi_graph(n, p=0.1, directed=False):
         return nx.gnp_random_graph(n, p)
     
 def generate_barabasi_albert_graph(n, m=10):
-    """_summary_
-    n : Le nombre de nœuds dans le graphe.
-    m : Le nombre d'arêtes à attacher de chaque nouveau nœud aux nœuds existants.
+    """
+        Generates a Barabási-Albert graph.
+
+        Parameters:
+        n (int): The number of nodes in the graph.
+        m (int): The number of edges to attach from a new node to existing nodes.
     """
     return nx.barabasi_albert_graph(n, m)
 
 def generate_watts_strogatz_graph(n, k=10, p=0.1):
-    """_summary_
-    n : Le nombre de nœuds dans le graphe.
-    k : Chaque nœud est connecté à k voisins proches dans un anneau.
-    p : La probabilité de réarranger chaque arête.
+    """
+        Generates a Watts-Strogatz small-world graph.
+
+        Parameters:
+        n (int): The number of nodes in the graph.
+        k (int): Each node is connected to k nearest neighbors in a ring topology.
+        p (float): The probability of rewiring each edge.
     """
     return nx.watts_strogatz_graph(n, k, p)
 
 def generate_random_bipartite_graph(n, m, p=0.1):
-    """_summary_
-    n : Le nombre de nœuds dans la première partition.
-    m : Le nombre de nœuds dans la deuxième partition.
-    p : La probabilité pour chaque paire de nœuds (un de chaque partition) d'être connectée par une arête.
+    """
+        Generates a random bipartite graph.
+
+        Parameters:
+        n (int): The number of nodes in the first partition.
+        m (int): The number of nodes in the second partition.
+        p (float): The probability for each pair of nodes (one from each partition) to be connected by an edge.
     """
     return nx.bipartite.random_graph(n, m, p)
 
 def generate_random_regular_graph(n, d=100):
-    """_summary_
-    d : Le degré de chaque nœud.
-    n : Le nombre de nœuds dans le graphe.
+    """
+        Generates a random regular graph.
+
+        Parameters:
+        d (int): The degree of each node.
+        n (int): The number of nodes in the graph.
     """
     return nx.random_regular_graph(d, n)
 
 def generate_powerlaw_graph(n, exponent=2.5):
-    """_summary_
-    n : Le nombre de nœuds dans le graphe.
-    exponent (optionnel) : L'exposant de la distribution de degré (paramètre par défaut est 2.5). Cependant, cette valeur n'est pas directement utilisée dans powerlaw_cluster_graph.
-    Note : Cette fonction utilise en fait deux autres paramètres : m, le nombre de nouvelles arêtes attachées à chaque nouveau nœud, et p, la probabilité de création d'un triangle. Les valeurs utilisées ici sont m=3 et p=0.1.
-    """
+    """_summary_"""
     return nx.powerlaw_cluster_graph(n, 3, 0.1)
 
 def fast_iso(G1,G2,result):
@@ -111,39 +124,39 @@ def measure_vf2(G1,G2,result):
 def compare_fast_iso(G1,G2,result,timeout_duration):
     process = multiprocessing.Process(target=measure_fast_iso,args=(G1,G2,result,))
     process.start()
-    process.join(timeout_duration)  # Attends la fin du processus avec un timeout
+    process.join(timeout_duration)  
 
     if process.is_alive():
         result["duration"]=timeout_duration
         result["memory"]=0
         result["iso_count"]=0
-        process.terminate()  # Terminer le processus s'il dépasse le timeout
+        process.terminate()
 
     return result
 
 def compare_vf2(G1,G2,result,timeout_duration):
     process = multiprocessing.Process(target=measure_vf2,args=(G1,G2,result,))
     process.start()
-    process.join(timeout_duration)# Attends la fin du processus avec un timeout
+    process.join(timeout_duration)
 
     if process.is_alive():
         result["duration"]=timeout_duration
         result["memory"]=0
         result["iso_count"]=0
-        process.terminate()  # Terminer le processus s'il dépasse le timeout
+        process.terminate()
 
     return result
 
 def compare_vf2pp(G1,G2,result,timeout_duration):
     process = multiprocessing.Process(target=measure_vf2pp,args=(G1,G2,result,))
     process.start()
-    process.join(timeout_duration)# Attends la fin du processus avec un timeout
+    process.join(timeout_duration)
 
     if process.is_alive():
         result["duration"]=timeout_duration
         result["memory"]=0
         result["iso_count"]=0
-        process.terminate()  # Terminer le processus s'il dépasse le timeout
+        process.terminate()
 
     return result
 
@@ -167,8 +180,14 @@ benchmarkx = {
     "Random_Regular":generate_random_regular_graph,
     "Powerlaw":generate_powerlaw_graph
     }
+
+def generate_shuffled_graph(G):
+    nodes = list(G.nodes)
+    random.shuffle(nodes)
+    mapping = {old_label: new_label for old_label, new_label in zip(G.nodes, nodes)}
+    return nx.relabel_nodes(G, mapping)
 #
-timeout_duration=3600
+timeout_duration=3600#1H
 result = multiprocessing.Manager().dict()
 
 # Parcourir les benchmarks et comparer les algorithmes
@@ -187,7 +206,7 @@ for name, generator in benchmarkx.items():
     #
     for size in range(500, 2000, 500):
         G1 = generator(size)
-        G2 = generator(size)
+        G2 = generate_shuffled_graph(G1)
         
         compare_vf2(G1, G2, result,timeout_duration)
         vf2_memory.append(result["memory"])
